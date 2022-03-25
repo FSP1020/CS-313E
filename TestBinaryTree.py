8d4e335f95ed8bc86d547ee3dfdc41f2d949fe60
Sam Pomajevich <47705880+FSP1020@users.noreply.github.com>
2022-03-25 11:18:44 -0500

    Update TestBinaryTree.py

--- TestBinaryTree.py
+++ TestBinaryTree.py
@@ -3,18 +3,6 @@
 #  Description: Takes in three possible trees and tests whether they
 #  are similar, prints a level of the tree, the height and the number of nodes.
 
-#  Student Name: Samuel Pomajevich
-
-#  Student UT EID: SRP2938
-
-#  Partner Name: Natania Christopher
-
-#  Partner UT EID: nnc476
-
-#  Course Name: CS 313E
-
-#  Unique Number: 50845
-
 #  Date Created: 11/19/2020
 
 #  Date Last Modified: 11/20/2020
